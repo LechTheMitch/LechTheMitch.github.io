0ac5fc9873c21ee6ca65ce29791059e4f2e9a482
LechTheMitch <104312143+LechTheMitch@users.noreply.github.com>
2024-04-29 02:19:39 +0300

    Finalization

---
+++ src/js/script.js
@@ -0,0 +1,43 @@
+// Sign Up Popup
+$(".openPopup").click(function () {
+    $(".popupOverlay, .popupContent").addClass("active")
+})
+$(".closepopup").click(function () {
+    $(".popupOverlay, .popupContent").removeClass("active")
+})
+
+//Log in Handler
+$(".openPopupLogin").click(function () {
+    $(".log-in-form").removeClass("hide")
+    $(".sign-up-form").addClass("hide")
+})
+$(".closePopupLogin").click(function () {
+    $(".log-in-form").addClass("hide")
+    $(".sign-up-form").removeClass("hide")
+})
+// Json Loader for Products
+let products = null;
+fetch('products.json')
+    .then(response => response.json())
+    .then(data => {
+        products = data;
+        addDataToHTML();
+    })
+
+function addDataToHTML() {
+    let listProductHTML = document.querySelector('.cardsLoader');
+
+    {
+        products.forEach(product => {
+            let newProduct = document.createElement('li');
+            newProduct.href = './details.html?id=' + product.id;
+            newProduct.classList.add('card');
+            newProduct.innerHTML =
+                `<a href="${newProduct.href}"><img src="${product.image}" alt="None">
+       <span>${product.name}</span>
+       <span class="price">${product.price} EGP</span></a>`;
+            listProductHTML.appendChild(newProduct);
+
+        });
+    }
+}
