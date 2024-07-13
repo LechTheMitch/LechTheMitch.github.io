5339432cd323dc6d4aeaf93c5f3f9aa00b1959f0
LechTheMitch <104312143+LechTheMitch@users.noreply.github.com>
2024-07-14 02:15:32 +0300

    Implement Read More Button for large blocks of text

--- docs/js/main.js
+++ docs/js/main.js
@@ -14,3 +14,16 @@
         right: "-300px",
     })
  })
+ $("#readmore").click(function(){
+    if($("#dots").css("display") == "none"){
+        $("#dots").css("display","inline");
+        $("#readmore").html("Read More");
+        $("#more-text").css("display","none");
+        return;
+    }
+    else{
+        $("#dots").css("display","none");
+        $("#readmore").html("Read Less");
+        $("#more-text").css("display","inline");
+    }
+ })
